a9dbc72dfac6b97172cc2706b3def03b82e72c86
Abhiraj T R <abhirajtr007@gmail.com>
2025-02-13 00:35:30 +0530

    chore: initialize shadcn-ui setup

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import LoginPage from "../pages/LoginPage"
 import { ROUTES } from "./routes"
+import HomePage from "@/pages/HomePage"
 
 const AppRoutes = () => {
     return (
         <Routes>
+            <Route path={ROUTES.HOME} element={<HomePage />} />
             {/* authentication */}
             <Route path={ROUTES.LOGIN} element={<LoginPage />} />
         </Routes>
