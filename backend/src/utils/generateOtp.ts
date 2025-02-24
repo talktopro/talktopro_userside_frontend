8265940240700d567a1d37c947c004738e3bd301
Sameen K A <147019872+Sameen-K-A@users.noreply.github.com>
2025-02-25 00:16:04 +0530

    Delete backend directory

--- backend/src/utils/generateOtp.ts
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-export const createOTP = (length = 6) => {
-  try {
-    return crypto.randomInt(10 ** (length - 1), 10 ** length).toString();
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
