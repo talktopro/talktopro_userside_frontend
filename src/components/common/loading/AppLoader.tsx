ac95ed14e3ee5b5ace2aa36512355550d89b0b1d
Sameen-K-A <sameensameen60@gmail.com>
2025-05-30 14:57:31 +0530

    Remove logo from loader screen (for live take some time to load logo, it affect UX)

--- src/components/common/loading/AppLoader.tsx
+++ src/components/common/loading/AppLoader.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import appLogo from '@/assets/svg/logo.svg';
 
 const AppLoader: React.FC = () => {
   return (
     <div className="min-h-screen homepage_keyFeatures_bg flex items-center justify-center p-4">
       <div className="text-center">
-        <div className="flex items-center justify-center gap-3">
-          <img
-            src={appLogo}
-            className='w-15 h-auto'
-          />
-          <h3 className="text-4xl font-bold flex items-center justify-center gap-1">
-            Talk<span>To</span> <span className="text-purple-500">Pro</span>
-          </h3>
-        </div>
+        <h3 className="text-4xl not-sm:text-3xl font-bold">
+          Talk<span>To</span> <span className="text-purple-500">Pro</span>
+        </h3>
 
         {/* Train Loading Animation */}
-        <div className="mt-5 w-70 max-w-full mx-auto">
-          <div className="relative bg-muted rounded-full h-1.5 overflow-hidden">
+        <div className="mt-5 w-70 not-sm:w-56 max-w-full mx-auto">
+          <div className="relative bg-gray-200 dark:bg-muted rounded-full h-1.5 overflow-hidden">
             <div className="absolute top-0 left-0 h-full w-2/5 bg-gradient-to-r from-purple-400 to-purple-600 rounded-full animate-train"></div>
           </div>
         </div>
