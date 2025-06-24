49dc1b94eaaa8a9865b11d35e691c4a97f01df4b
emil shiju <emilshiju10@gmail.com>
2025-06-24 16:22:01 +0530

    working on the searchbar

--- src/components/user/MobileSearchBar.tsx
+++ src/components/user/MobileSearchBar.tsx
@@ -44,13 +44,14 @@ const MobileSearchbar = ({ closeSearchBar }: MobileSearchbarProps) => {
   const closeSearch = () => {
     setSearchValue("");
     setMentorsList([]);
-    closeSearchBar()
+    closeSearchBar();
   };
 
   return (
-  <div className="max-w-md sm:max-w-sm w-full mx-auto px-4 relative md:hidden mb-4" ref={searchRef}>
-
-
+    <div
+      className="max-w-md sm:max-w-sm w-full mx-auto px-4 relative md:hidden mb-4"
+      ref={searchRef}
+    >
       <div className="relative">
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <Search
@@ -62,7 +63,7 @@ const MobileSearchbar = ({ closeSearchBar }: MobileSearchbarProps) => {
         <Input
           type="text"
           placeholder="Search mentors..."
-          className="w-full pl-10 pr-10 py-5 rounded-xl bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 focus-visible:ring-2 focus-visible:ring-primary/50"
+          className="w-full pl-10 pr-10 py-5 rounded-sm    focus-visible:ring-2 focus-visible:ring-primary/50"
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
         />
@@ -77,14 +78,12 @@ const MobileSearchbar = ({ closeSearchBar }: MobileSearchbarProps) => {
       </div>
 
       {searchValue.length > 0 && (
-        
- <>
+        <>
           <div
             className="fixed inset-0 bg-black/50 dark:bg-black/70  backdrop-blur-sm z-40"
             onClick={closeSearch}
-            style={{ top: "var(--navbar-height, 72px)",marginTop: "3rem" }}
+            style={{ top: "var(--navbar-height, 72px)", marginTop: "3rem" }}
           />
-          
 
           <div className="fixed top-18 left-1/2 p-4 transform -translate-x-1/2 mt-14 bg-background border rounded-lg shadow-xl z-50 max-w-screen overflow-hidden ">
             {isSearching ? (
@@ -94,9 +93,9 @@ const MobileSearchbar = ({ closeSearchBar }: MobileSearchbarProps) => {
                 ))}
               </div>
             ) : mentorsList.length === 0 ? (
-              <div className="flex flex-col justify-center items-center h-72 w-4xl">
+              <div className="flex flex-col justify-center items-center h-32 w-75">
                 <GraduationCap
-                  strokeWidth={1} 
+                  strokeWidth={1}
                   size={50}
                   className="opacity-60"
                 />
@@ -116,7 +115,6 @@ const MobileSearchbar = ({ closeSearchBar }: MobileSearchbarProps) => {
             )}
           </div>
         </>
-
       )}
     </div>
   );
